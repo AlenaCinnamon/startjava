fbc185764a184e4eb1fd0a23fae980aa4824510f
KirichenkoEA <ekirichenko12@gmail.com>
2023-07-30 19:51:33 +0300

    Инициализация проекта
    -добавл файл с описание проекта
    -добавил реализацию первого класса

--- HelloPet.java
+++ HelloPet.java
@@ -1,5 +1,5 @@
 public class HelloPet {
             public static void main(String[] args) {
-            System.out.println("Hello!I am a little cat!");
+            System.out.println("Hello!I am a little mew cat!");
     }
 }
