e7896e81b5b7fd87d7724842ed888a8a3796359d
KirichenkoEA <ekirichenko12@gmail.com>
2023-07-30 18:57:13 +0300

    cat changers

--- HelloPet.java
+++ HelloPet.java
@@ -1,6 +1,6 @@
 public class HelloPet {
             public static void main(String[] args) {
-            System.out.println("Hello Cat!!!!");
+            System.out.println("Hello! Cat!");
 
     }
 
