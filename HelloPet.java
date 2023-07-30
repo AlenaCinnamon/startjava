b53db12277d6b5f340354aa26278d90c11e27025
KirichenkoEA <ekirichenko12@gmail.com>
2023-07-30 19:05:39 +0300

    чот сделали

--- HelloPet.java
+++ HelloPet.java
@@ -1,7 +1,5 @@
 public class HelloPet {
             public static void main(String[] args) {
-            System.out.println("Hello! Cat!");
-
+            System.out.println("Hello!I am a cat!");
     }
-
 }
